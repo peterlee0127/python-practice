aa916732e61a9af7906686db852e8c9165d07c51
Peter Lee <peterlee0127@gmail.com>
2018-08-22 23:05:59 +0800

    update

--- clock.py
+++ clock.py
@@ -4,13 +4,14 @@ def check(num):
     r = [perm for perm in itertools.permutations(num)]
     max = 0
     for item in r:
-        hour = str(item[0])+str(item[1])
-        time = str(item[2])+str(item[3])
-        if(int(hour)<=24 and int(time) <= 60):
-            result = int(hour)*100+int(time)
+        hour = item[0]*10+item[1]
+        time = item[2]*10+item[3]
+        if(hour<=24 and time <= 60):
+            result = hour*100+time
             if result > max:
                 max = result
     return max
 
 print(check([1,2,3,4]))
 print(check([5,3,2,1]))
+print(check([5,3,4,6]))
